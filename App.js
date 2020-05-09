8106d4167e8e628f9874b9d4e4e88e344f81463c
zdmc23 <191707+zdmc23@users.noreply.github.com>
2020-05-09 12:36:17 -0400

    Install TensorFlow.js \ https://www.npmjs.com/package/@tensorflow/tfjs-react-native/v/0.2.1 \ https://stackoverflow.com/questions/55484740/how-to-append-extension-in-metro-config-js-for-metro-bundler

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as tf from '@tensorflow/tfjs';
+import '@tensorflow/tfjs-react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isTfReady: false,
+    };
+  }
+
+  async componentDidMount() {
+    // Wait for tf to be ready.
+    await tf.ready();
+    // Signal to the app that tensorflow.js can now be used.
+    this.setState({
+      isTfReady: true,
+    });
+  }
+
+  render() {
+    return(
+      <View style={styles.container}>
+        <Text>TensorFlow.js successfully installed!</Text>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,3 +37,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
---
+++ metro.config.js
@@ -0,0 +1,28 @@
+// get defaults assetExts array
+const defaultAssetExts = require("metro-config/src/defaults/defaults").assetExts;
+
+// Change 1 (import the blacklist utility)
+const blacklist = require('metro-config/src/defaults/blacklist');
+ 
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+  resolver: {
+    // Change 2 (add 'bin' to assetExts)
+    assetExts: [
+      ...defaultAssetExts,
+      'bin',
+      'txt',
+      'jpg'
+    ],
+    sourceExts: ['js', 'json', 'ts', 'tsx', 'jsx'],
+    // Change 3 (add platform_node to blacklist)
+    blacklistRE: blacklist([/platform_node/])
+  },
+};
